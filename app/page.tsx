363da0aa58fd51a6498a8c5e8392f538ddab5262
MINGW64 <jherdiditaa@gmail.com>
2026-05-15 22:45:59 +0700

    make background transparant

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Lanyard from "@/components/ui/Lanyard";
+import Lanyard from "@/components/ui/Lanyard"; 
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-950 min-h-screen text-zinc-50 font-sans">
-      {/* Container wajib dengan tinggi pasti agar objek 3D Lanyard dapat merender */}
+    // Ubah bagian ini menjadi bg-transparent agar latar belakangnya hilang
+    <div className="flex flex-col flex-1 items-center justify-center bg-transparent min-h-screen">
       <div className="w-full h-[500px] max-w-md flex items-center justify-center">
-        <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+        <Lanyard position={[0, 0, 12]} gravity={[0, -40, 0]} transparent={true} />
       </div>
     </div>
   );
